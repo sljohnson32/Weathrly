f22afed326fc8f7be970949915475fb4176c611c
sljohnson32 <sljohnson32@gmail.com>
2016-12-11 10:26:50 -0700

    hide hourly details by default

--- lib/main.jsx
+++ lib/main.jsx
@@ -50,10 +50,9 @@ export default class Main extends React.Component {
       </div>
     );
   }
-}
+};
 
 const WeatherInfo = ( {data} ) => {
-  render() {
     if (data === null) {
       return (
         <div>
@@ -72,32 +71,37 @@ const WeatherInfo = ( {data} ) => {
       </ul>
     </div>
   );
-  }
 }
 
-const DayWeather = (props) => {
-  const { date, hourly, temp, weatherType } = props;
-  let ShowDetails = false;
-
+export class DayWeather extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showDetails: false,
+    };
+  }
   showHideDetails() {
-    ShowDetails = !ShowDetails;
+    this.setState( {showDetails: !this.state.showDetails} )
   }
 
+
   render() {
+    const { date, hourly, temp, weatherType } = this.props;
     return (
       <div>
-        <section
-          className='DayOverview'
-          onClick=
-            {this.showHideDetails.bind(this);
-          }
-          >
+        <section className='DayOverview'>
           <h2>{ date }</h2>
+          <button
+            className="ShowDetails-button"
+            onClick=
+              {this.showHideDetails.bind(this)
+            }
+          >Show Details</button>
             <p>{ "Today's Summary: " + weatherType.type + " with a high of " + temp.high + " and a low of " + temp.low + "."}</p>
           <article className='HighLows'>
             <h4>{ "Today's high: " + temp.high }</h4>
             <h4>{ "Today's low: " + temp.low }</h4>
-            <DayDetails {...hourly} display={showDetails} />
+            <DayDetails {...hourly} display={this.state.showDetails} />
           </article>
         </section>
       </div>
@@ -105,39 +109,32 @@ const DayWeather = (props) => {
   }
 }
 
-
 const DayDetails = (props, display) => {
+  console.log(display);
   let timeBreakDown = props.timeBreakDown;
   let displayStatus = display;
-
-  render() {
-    if (!displayStatus) {
-      return <div></div>
-    }
-
-    return (
-      <div>
-        <aside className='DayDetails'>
-          <ul>
-            {timeBreakDown.map((hour, index) => {
-              let value = <HourlyDetails key={index} {...timeBreakDown} />
-              return value;
-            })}
-          </ul>
-        </aside>
-      </div>
-    )
-  }
+  if (displayStatus) {
+    return <div></div>
+  } else return (
+    <div>
+      <aside className='DayDetails'>
+        <ul>
+          {timeBreakDown.map((hour, index) => {
+            let value = <HourlyDetails key={index} {...timeBreakDown} />
+            return value;
+          })}
+        </ul>
+      </aside>
+    </div>
+  )
 }
 
 const HourlyDetails = (props) => {
-  render() {
-    return (
-      <div>
-        hi
-      </div>
-    )
-  }
+  return (
+    <div>
+      hi
+    </div>
+  )
 }
 
 ReactDOM.render(<Main />, document.getElementById('app'));
